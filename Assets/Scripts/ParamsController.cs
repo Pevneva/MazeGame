a39e3bb9349a243fb1e7bbfe9808358734eb2347
Lyudmila5733 <millo4ka@list.ru>
2022-02-19 23:56:24 +0300

    Added trail.

---
+++ Assets/Scripts/ParamsController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ParamsController
+{
+    public const float DelayDeath = 0.35f;
+    public const float DelayStopMoving = 0.35f;
+}
